9ff9d2767717210775a7489861f46e98be9b56be
Ryan Street <ryanstreet122@gmail.com>
2023-01-11 14:57:33 -0500

    dashboard rendering

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -83,7 +83,23 @@ router.get('/signup', (req,res) =>{
     res.render('signup')
 });
 
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: {exclude: ['password']},
+            include: [{model: Blog}]
+        })
+        const userProfile = userData.get({plain: true})
 
+        res.render("dashboard", {
+            ...userProfile,
+            loggedIn: true
+
+        })
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 
 module.exports = router;
